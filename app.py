798d7a0a6b6f63aae3e3994fb9632b8d22ba4764
ThiagoAguirre <thiago.aguirre08@outlook.com>
2024-06-13 00:36:24 -0300

    new features

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [{
+        'id': 1,
+        'title': 'Data Analytic',
+        'local': 'SP, Brazil',
+        'salario': 'R$ 5.000'
+    },
+    {
+        'id': 2,
+        'title': 'Flask Developer',
+        'local': 'SP, Brazil',
+        'salario': 'R$ 5.000'
+    },
+    {
+        'id': 3,
+        'title': 'Node.js Developer',
+        'local': 'SP, Brazil',
+        'salario': 'R$ 5.000'
+    },
+    {
+        'id': 4,
+        'title': 'Ruby Developer',
+        'local': 'SP, Brazil',
+        'salario': 'R$ 5.000'
+    },
+    {
+        'id': 5,
+        'title': 'Python Developer',
+        'local': 'SP, Brazil',
+        'salario': 'R$ 5.000'
+    }
+]
 
 @app.route("/")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
 
+@app.route('/jobs')
+def listJobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(debug=True)
